1eef0ffc294b0910354fbe245e2f7a9e4e1de879
Your Name <you@example.com>
2024-04-23 14:19:12 -0700

    secound-commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -33,9 +33,9 @@ const infoData = [
 
 
 const About = () => {
-  const title = "section-title mb-8 xl:mb-8 text-center mx-auto";
+  const title = "section-title mb-8 xl:mb-8 text-center mx-auto  ";
   return (
-    <section className="pb-12 xl:py-24 min-h-screen">
+    <section className="pb-12 xl:py-24 min-h-screen  overflow-hidden">
       <div className="container mx-auto">
         <Fade direction='up' delay={400} cascade damping={1e-1} triggerOnce={true}>
           <h2 className={title}>About me</h2>
@@ -45,7 +45,7 @@ const About = () => {
           <div className='relative flex-1 xl:flex'>
             <Fade direction='left' delay={600} cascade damping={1e-1} triggerOnce={true}>
               <HeroImg imgSrc='https://res.cloudinary.com/djhjt07rh/image/upload/v1713904914/437585332_976969253793270_3287469938990990648_n_dtckaj.jpg' 
-              containerStyles='relative' />
+              containerStyles='relative overflow-x-hidden' />
             </Fade>
           </div>
           <div className="flex-1">
