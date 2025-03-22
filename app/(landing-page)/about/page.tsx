0fafe85f8558d6632ccccdbd6cf146df9e5b46df
Jane Doe <jane.doe@example.com>
2025-03-22 20:13:44 +0600

    shohidul-pramani

--- app/(landing-page)/about/page.tsx
+++ app/(landing-page)/about/page.tsx
@@ -114,7 +114,7 @@
 'use client';
 
 import { Fade } from "react-awesome-reveal";
-import { RiUserHeartFill, RiStarHalfSLine, RiVidiconFill, RiVipCrown2Fill, Ri24HoursFill, RiCodeSSlashFill } from "react-icons/ri";
+import { RiUserHeartFill, RiStarHalfSLine, RiVidiconFill, RiVipCrown2Fill, Ri24HoursFill} from "react-icons/ri";
 import Image from "next/image";
 
 const infoData = [
@@ -125,117 +125,205 @@ const infoData = [
   { icon: <Ri24HoursFill size={20} />, text: 'Sherpur, Dhunat, Bogura', name: 'Location:' },
 ];
 
-const skills = ["React.js", "Next.js", "Node.js", "MongoDB", "TailwindCSS", "TypeScript", "Redux", "REST API"];
+const Frontend = ["React.js", "JavaScript", "Next.js", "(ES6+)", "TailwindCSS", "TypeScript", "Material UI", "Redux", "Bootstrap", "Framer Motion"];
+const Backend = ["Node.js", "express.js", "MongoDB", "Firebase", "REST APIs", "NextAuth"];
+const Version = ["Git", "GitHub", "Vercel", "Netlify"];
+const Other = ["JWT Authentication", "Payment Integration (Stripe)", "Performance Optimization", "Debugging & Problem Solving"]
 
-const experience = [
-  {
-    title: "MERN Stack Developer",
-    company: "Freelance",
-    duration: "2021 - Present",
-    description: "Developing high-performance full-stack applications using the MERN stack, including real-time features, authentication, and optimized UI/UX."
-  },
-];
+// const experience = [
+//   {
+//     title: "MERN Stack Developer",
+//     company: "Freelance",
+//     duration: "2021 - Present",
+//     description: "Developing high-performance full-stack applications using the MERN stack, including real-time features, authentication, and optimized UI/UX."
+//   },
+// ];
 
-const projects = [
-  {
-    name: "JobFinder App",
-    description: "A job listing platform with user authentication, job postings, and real-time chat using WebSockets.",
-    link: "#"
-  },
-  {
-    name: "LinkedIn Clone",
-    description: "A professional networking platform with user profiles, posts, and follow features.",
-    link: "#"
-  },
-];
+// const projects = [
+//   {
+//     name: "JobFinder App",
+//     description: "A job listing platform with user authentication, job postings, and real-time chat using WebSockets.",
+//     link: "#"
+//   },
+//   {
+//     name: "LinkedIn Clone",
+//     description: "A professional networking platform with user profiles, posts, and follow features.",
+//     link: "#"
+//   },
+// ];
 
 const About = () => {
   return (
-    <section className="container mx-auto px-4">
-      <div className="py-6 px-6 mb-4 border-b-2 border-red-400 shadow-lg dark:bg-gray-900 dark:text-white rounded-lg">
-        <Fade direction="up" delay={300} cascade damping={0.1} triggerOnce>
-          <h2 className="text-4xl flex items-center justify-center font-bold text-center bg-gradient-to-r from-blue-600 to-indigo-400  text-transparent bg-clip-text">
-            About Me
+    <section className="container w-full -translate-y-8 relative overflow-hidden">
+      <div className="border-b-2 border-red-400 shadow-lg text-black rounded-lg p-6">
+        <Fade direction="up" delay={250} cascade damping={0.1} triggerOnce>
+          <h2 className="text-3xl md:text-4xl pb-5 flex items-center justify-center font-bold text-center bg-gradient-to-r from-blue-600 to-indigo-400 text-transparent bg-clip-text">
+            Shohidul Pramanik
           </h2>
         </Fade>
-
-        <div className="flex flex-col gap-10 lg:flex-row items-center mt-6">
-          {/* Image */}
-          <Fade direction="left" delay={400} triggerOnce>
-            <div className="relative w-52 h-52 md:w-64 md:h-64 lg:w-80 lg:h-80">
-              <Image width={500} height={500} className="rounded-full border-4 border-blue-400 shadow-md" alt="Shohidul Pramanik"
-                src="https://res.cloudinary.com/djhjt07rh/image/upload/v1741590989/shoihid_xejbaq.png" />
-            </div>
-          </Fade>
-
-          {/* About Text */}
-          <Fade direction="right" delay={500} triggerOnce>
-            <div className="flex-1 text-lg">
-              <p className="text-gray-700 dark:text-gray-300">
-                I am a passionate <span className="font-bold text-blue-500">MERN Stack Developer</span> dedicated to crafting high-performance, user-friendly web applications.
-                With expertise in both frontend and backend development, I create scalable solutions with a focus on optimization and modern design principles.
-              </p>
-            </div>
-          </Fade>
-        </div>
-
-        {/* Info */}
-        <div className="mt-6">
-          <Fade direction="right" delay={600} cascade damping={0.1} triggerOnce>
-            {infoData.map((item, index) => (
-              <div key={index} className="flex items-center gap-2 text-lg mb-2">
-                <div className="text-primary">{item.icon}</div>
-                <span className="font-bold">{item.name}</span>
-                <span>{item.text}</span>
+        <Fade direction="up" delay={250} cascade damping={0.1} triggerOnce>
+          <h2 className="md:text-3xl text-xl flex items-center justify-center font-bold text-center bg-gradient-to-r from-blue-600 to-indigo-400 text-transparent bg-clip-text">
+            MERN Stack Developer | Frontend & Backend Expert
+          </h2>
+        </Fade>
+        {/* 60% - 40% Grid */}
+        <div className="grid grid-cols-1 md:grid-cols-3 mt-10 gap-6">
+          {/* Image (40%) */}
+          <div className="flex items-center justify-center md:col-span-1 order-first">
+            <Fade direction="right" delay={400} triggerOnce>
+              <div className="relative w-52 h-52 md:w-64 md:h-64 flex items-center justify-center border-4 border-blue-400 rounded-full overflow-hidden">
+                <Image
+                  width={500}
+                  height={500}
+                  className="rounded-full w-full h-full bg-white object-cover"
+                  alt="Shohidul Pramanik"
+                  src="https://res.cloudinary.com/djhjt07rh/image/upload/v1741590989/shoihid_xejbaq.png"
+                  priority
+                />
               </div>
-            ))}
-          </Fade>
+            </Fade>
+          </div>
+          {/* About Text (60%) */}
+          <div className="md:col-span-2 flex items-center">
+            <Fade direction="left" delay={350} triggerOnce>
+              <p className="text-lg   leading-relaxed text-black">
+                I am a passionate <span className="font-bold text-blue-500">MERN Stack Developer</span>,A highly skilled MERN Stack Developer with 4 years of experience in building scalable web applications. Proficient in
+                React.js, Next.js, Node.js, and MongoDB. Passionate about developing high-performance, user-friendly, and visually
+                appealing applications. Experienced in API integration, authentication systems, and performance optimization. Always
+                eager to take on new challenges and contribute to dynamic teams.
+              </p>
+            </Fade>
+          </div>
         </div>
 
-        {/* Skills */}
-        <div className="mt-6">
-          <Fade direction="up" delay={700} triggerOnce>
-            <h3 className="text-2xl font-semibold mb-2">Skills</h3>
-            <div className="flex flex-wrap gap-3">
-              {skills.map((skill, index) => (
-                <span key={index} className="px-3 py-1 bg-blue-500 text-white rounded-md shadow-md">
-                  {skill}
+        <div className="flex flex-col gap-2 mt-5">
+          {/* Skills */}
+          <div className="text-4xl font-bold">
+            Technical Skills
+
+          </div>
+          {/* frontend */}
+          <div className="">
+            <Fade direction="up" delay={700} triggerOnce>
+              <h3 className="text-2xl font-semibold mb-2">Frontend</h3>
+              <div className="flex flex-wrap gap-3">
+                {Frontend.map((skill, index) => (
+                  <span key={index} className="px-3 py-1 bg-blue-500 text-white rounded-md shadow-md">
+                    {skill}
+                  </span>
+                ))}
+              </div>
+            </Fade>
+          </div>
+          <div className="">
+            <Fade direction="up" delay={700} triggerOnce>
+              <h3 className="text-2xl font-semibold mb-2">Backend</h3>
+              <div className="flex flex-wrap gap-3">
+                {Backend.map((skill, index) => (
+                  <span key={index} className="px-3 py-1 bg-blue-500 text-white rounded-md shadow-md">
+                    {skill}
+                  </span>
+                ))}
+              </div>
+            </Fade>
+          </div>
+          <div className="">
+            <Fade direction="up" delay={700} triggerOnce>
+              <h3 className="text-2xl font-semibold mb-2">Version Control & Deployment:</h3>
+              <div className="flex flex-wrap gap-3">
+                {Version.map((skill, index) => (
+                  <span key={index} className="px-3 py-1 bg-blue-500 text-white rounded-md shadow-md">
+                    {skill}
+                  </span>
+                ))}
+              </div>
+            </Fade>
+          </div>
+          <div className="">
+            <Fade direction="up" delay={700} triggerOnce>
+              <h3 className="text-2xl font-semibold mb-2">Other:</h3>
+              <div className="flex flex-wrap gap-3">
+                {Other.map((skill, index) => (
+                  <span key={index} className="px-3 py-1 bg-blue-500 text-white rounded-md shadow-md">
+                    {skill}
+                  </span>
+                ))}
+              </div>
+            </Fade>
+          </div>
+          <div className="">
+            <Fade direction="up" delay={700} triggerOnce>
+              <h3 className="text-2xl font-semibold mb-2">Professional Experience:</h3>
+              <div className="flex flex-wrap gap-3">
+                <span className="px-3 py-1 bg-blue-500 text-white rounded-md shadow-md">
+                  MERN Stack Development: Designed and developed multiple full-stack applications using React.js, Next.js, and
+                  MongoDB, ensuring scalability and high performance.
+                  Responsive UI/UX Development: Created mobile-first and pixel-perfect web applications using Tailwind CSS, Material
+                  UI, and Framer Motion.
+                  API Integration & Backend Development: Implemented REST APIs, user authentication (JWT, NextAuth), and third-party
+                  API integrations.
+                  Performance Optimization & Debugging: Improved application performance by 30% using caching, lazy loading, and
+                  optimized API calls.
                 </span>
-              ))}
-            </div>
-          </Fade>
-        </div>
 
-        {/* Experience */}
-        <div className="mt-6">
+              </div>
+            </Fade>
+          </div>
+
+          <div className="">
+            <Fade direction="up" delay={700} triggerOnce>
+              <h3 className="text-2xl font-semibold mb-2">Education:</h3>
+              <div className="flex flex-wrap gap-3">
+                <span className="px-3 py-1 bg-blue-500 text-white rounded-md shadow-md">
+                Bachelor of Science (Honours) in Zoology (Ongoing, Gov. Shariakandi Degree College, Bogura, 2023 - Present)
+                Higher Secondary Certificate (HSC) - Science (Sherpur College, Bogura, 2022)
+                </span>
+
+              </div>
+            </Fade>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+
+
+
+  );
+};
+
+export default About;
+
+
+
+
+
+{/* Experience */ }
+{/* <div className="mt-6">
           <Fade direction="up" delay={800} triggerOnce>
             <h3 className="text-2xl font-semibold mb-2">Experience</h3>
             {experience.map((exp, index) => (
-              <div key={index} className="border-l-4 border-blue-400 pl-4 mb-4">
+              <div key={index} className="border-l-4 border-blue-400 ">
                 <h4 className="text-xl font-semibold">{exp.title} - {exp.company}</h4>
                 <span className="text-gray-600 dark:text-gray-400">{exp.duration}</span>
                 <p className="text-gray-700 dark:text-gray-300">{exp.description}</p>
               </div>
             ))}
           </Fade>
-        </div>
+        </div> */}
 
-        {/* Projects */}
-        <div className="mt-6">
+{/* Projects */ }
+{/* <div className="mt-6">
           <Fade direction="up" delay={900} triggerOnce>
             <h3 className="text-2xl font-semibold mb-2">Projects</h3>
             {projects.map((project, index) => (
-              <div key={index} className="border-l-4 border-green-400 pl-4 mb-4">
+              <div key={index} className="border-l-4 border-green-400 ">
                 <h4 className="text-xl font-semibold">{project.name}</h4>
                 <p className="text-gray-700 dark:text-gray-300">{project.description}</p>
                 <a href={project.link} className="text-blue-500 underline">View Project</a>
               </div>
             ))}
           </Fade>
-        </div>
-      </div>
-    </section>
-  );
-};
-
-export default About;
+        </div> */}
--- app/(landing-page)/profile/page.tsx
+++ app/(landing-page)/profile/page.tsx
@@ -30,7 +30,7 @@ const Profile = () => {
   if (!isAuthChecked || checkAuthLoader) return <p>Loading...</p>;
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 p-6">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
       <motion.div
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
